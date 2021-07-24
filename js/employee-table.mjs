462b5f4b5ec3415f832920f2b4f97d9d2656ff4f
Varga Zsolt <shadowvzs@hotmail.com>
2021-07-24 21:43:15 +0300

    removed the unused code

--- js/employee-table.mjs
+++ js/employee-table.mjs
@@ -9,20 +9,6 @@ if (document.readyState === 'complete') {
     window.addEventListener('load', init);
 }
 
-function showHideSalary() {
-    const elements = document.getElementsByClassName('salary');
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.toggle('hideSalary');
-    }
-
-    // change button text
-    const showHideButton = document.getElementById('showHideSalary');
-    const hiddenSalaries = document.getElementsByClassName('hideSalary');    
-    showHideButton.innerText = hiddenSalaries.length ? 'Show Salary' :'Hide Salary';
-}
-
-window.showHideSalary = showHideSalary;
-
 // application start here
 function init() {
     // remove event listener, we not need anymore
